5cbbe0f0959f98da9c74f929a30085c238dcc797
Calberk <park.edmund@gmail.com>
2018-12-20 13:26:31 -0800

    setup initial express server

--- index.js
+++ index.js
@@ -3,3 +3,20 @@ const cors = require('cors');
 const {resolve} = require('path');
 const PORT = process.env.PORT || 9000;
 
+const app = express();
+
+app.use(cors());  //more for development purposes
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// app.use(express.static());
+
+app.get('/', (req, res) => {
+    res.send('<h1>Server Running!</h1>');
+});
+
+app.listen(PORT, () =>{
+    console.log('Server runnign on PORT: ' + PORT);
+}).on('error', (err) => {
+    console.log('Listen Error: You already have a server running on PORT: ', PORT);
+});
