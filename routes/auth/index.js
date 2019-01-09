694aa86b892b481fb63e369cbeb2911676f51c25
Calberk <park.edmund@gmail.com>
2019-01-09 13:51:06 -0800

    initial local login working

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 const {signUp} = require('../../controllers/auth')
+const {requireSignIn} = require('../setup')
 
 router.post('/sign-up', signUp);
+
+router.post('/sign-in', requireSignIn, (req, res) => {
+    res.send({
+        success: true,
+        message: 'This is for sign-in',
+        user: req.user
+    })
+})
 
 module.exports = router;
