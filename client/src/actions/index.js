7077232f4ffa7109e49ba2f3f876d79bf9be4d4d
Calberk <park.edmund@gmail.com>
2019-01-15 14:41:14 -0800

    adjust axios call

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -16,7 +16,7 @@ export const jwtSignIn = () => async dispatch => {
 
 export const signUp = userInfo => async dispatch => {
     try {
-        const resp = await axios.post('/auth/sign-up', userInfo);
+        const resp = await axios.post('/auth/sign-up', userInfo);  
 
         console.log('Sign Up Resp: ', resp);
     } catch(err){
